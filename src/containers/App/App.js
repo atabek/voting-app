121932f8e713fd71aac561c9c7e4f784a186797e
Excellent Nomad <excellent.nomad@gmail.com>
2018-06-04 11:05:39 +0600

    code refactor for voting app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 
-import TimersDashboard from '../../components/TimersDashboard/TimersDashboard';
-
 class App extends Component {
   render() {
     return (
       <div id="main" class="main ui">
         <h1 class="ui dividing centered header">Timers</h1>
-        <TimersDashboard />
       </div>
     );
   }
